13051daead012e6357cae2b4f3b43c6851fd08a4
Blowser <ig.cortez@duocuc.cl>
2024-10-31 21:37:38 -0300

    Update login.page.ts
    se genera page home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  appTitle: string = 'SkeletonApp';
+  welcomeMessage: string = '¡Bienvenido!';
+  subtitleMessage: string = 'Inicia sesión';
+  exploreMessage: string = 'Explora la app';
+  footerText: string = 'Aquí comienza todo';
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
