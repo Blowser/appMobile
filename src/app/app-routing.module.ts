9d8ede8c718a4a4a4f9a2a187069bc213b50593f
Blowser <ig.cortez@duocuc.cl>
2024-10-31 23:19:44 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,31 +4,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login/ingresar',
+    pathMatch: 'full'
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+    path: 'login/ingresar',
+    loadChildren: () => import('./login/ingresar/ingresar.module').then(m => m.IngresarPageModule)
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'ingresar',
-    loadChildren: () => import('./login/ingresar/ingresar.module').then( m => m.IngresarPageModule)
-  },
-  {
-    path: 'registrarse',
-    loadChildren: () => import('./login/registrarse/registrarse.module').then( m => m.RegistrarsePageModule)
+    path: 'login/registrarse',
+    loadChildren: () => import('./login/registrarse/registrarse.module').then(m => m.RegistrarsePageModule)
   }
-
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
