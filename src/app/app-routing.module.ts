1ea6079bf8072ebe56cec328cf240eede44bbd87
Blowser <ig.cortez@duocuc.cl>
2024-11-01 13:52:43 -0300

    Update app-routing.module.ts
    se agrega home al routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login/ingresar',
+    redirectTo: 'login/ingresar',  /*AQUI EMPIEZA LA APP*/
     pathMatch: 'full'
   },
   {
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login/registrarse',
     loadChildren: () => import('./login/registrarse/registrarse.module').then(m => m.RegistrarsePageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   }
 ];
 
