d355d8c03034d08f6d124f877a6f6f27862124a1
Blowser <ig.cortez@duocuc.cl>
2024-11-01 21:29:33 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  data: any = { usuario: '' };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private activeRoute: ActivatedRoute, private router: Router) {
+    this.activeRoute.queryParams.subscribe(() => {
+      const navigation = this.router.getCurrentNavigation();
+      if (navigation && navigation.extras && navigation.extras.state) {
+        console.log('Datos recibidos en Home:', navigation.extras.state); // Agregado para depurar
+        if (navigation.extras.state.usuario) {
+          this.data.usuario = navigation.extras.state.usuario;
+        } else {
+          console.warn('La clave "usuario" no existe en extras.state');
+          this.router.navigate(['/login/ingresar']);
+        }
+      } else {
+        console.error('No se encontraron datos en extras.state');
+        this.router.navigate(['/login/ingresar']);
+      }
+    });
   }
 
+  ngOnInit() {}
 }
+
+
