1765629f638a403f61673d0b91cdfe5d2989fcf4
Blowser <ig.cortez@duocuc.cl>
2024-11-01 21:29:21 -0300

    Update ingresar.page.ts

--- src/app/login/ingresar/ingresar.page.ts
+++ src/app/login/ingresar/ingresar.page.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-ingresar',
   templateUrl: './ingresar.page.html',
   styleUrls: ['./ingresar.page.scss'],
 })
-export class IngresarPage {} // Asegúrate de que esta clase esté presente y exportada
+export class IngresarPage {
+  user = {
+    usuario: '',
+    password: ''
+  };
+
+  constructor(private router: Router) {}
+
+  ingresar() {
+    if (this.user.usuario.length >= 3 && this.user.usuario.length <= 8 && /^\w+$/.test(this.user.usuario)) {
+      if (this.user.password.length === 4 && /^\d+$/.test(this.user.password)) {
+        const navigationExtras: NavigationExtras = {
+          state: {
+            usuario: this.user.usuario // Asegúrate de pasar los datos de usuario correctamente
+          }
+        };
+        this.router.navigate(['/home'], navigationExtras);
+      } else {
+        alert('La contraseña debe tener 4 dígitos numéricos.');
+      }
+    } else {
+      alert('El usuario debe tener entre 3 y 8 caracteres alfanuméricos.');
+    }
+  }
+}
+
 
