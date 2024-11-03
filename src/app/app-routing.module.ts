b5fe257c1eb96dffdcf48b265c2cee2b00e0cb5e
Blowser <ig.cortez@duocuc.cl>
2024-11-02 23:12:40 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,11 +25,7 @@ const routes: Routes = [
   },
   {
     path: 'modificarperfil',
-    loadChildren: () => import('./explorador/modificarperfil/modificarperfil.module').then( m => m.ModificarperfilPageModule)
-  },
-  {
-    path: 'modificarperfil',
-    loadChildren: () => import('./explorador/modificarperfil/modificarperfil.module').then( m => m.ModificarperfilPageModule)
+    loadChildren: () => import('./explorador/modificarperfil/modificarperfil.module').then( m => m.ModificarPerfilPageModule)
   }
 ];
 
