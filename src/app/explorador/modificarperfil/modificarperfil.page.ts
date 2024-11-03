9430e9918b1a13661433f6cbbea5143f9d770b05
Blowser <ig.cortez@duocuc.cl>
2024-11-02 23:33:54 -0300

    Update modificarperfil.page.ts

--- src/app/explorador/modificarperfil/modificarperfil.page.ts
+++ src/app/explorador/modificarperfil/modificarperfil.page.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-modificarperfil',
   templateUrl: './modificarperfil.page.html',
   styleUrls: ['./modificarperfil.page.scss'],
 })
-export class ModificarPerfilPage {
+export class ModificarPerfilPage implements OnInit {
   data = {
-    usuario: 'usuarioEjemplo', // Valor inicial de ejemplo, cámbialo según tu lógica
-    password: 'contraseñaEjemplo', // Valor inicial de ejemplo, cámbialo según tu lógica
-    nombre: '',
-    apellido: '',
-    nivelEducacion: '',
-    fechaNacimiento: ''
+    usuario: '', // Se llenará con el usuario ingresado anteriormente
+    password: '', // Se llenará con la contraseña ingresada anteriormente
   };
 
-  constructor() { }
-}
+  informacion = {
+    nombre: '', // Se llenará con el nombre ingresado anteriormente
+    apellido: '', // Se llenará con el apellido ingresado anteriormente
+    nivelEducacion: '', // Se llenará con el nivel de educación
+    fechaNacimiento: '' // Se llenará con la fecha de nacimiento
+  };
+
+    ngOnInit() {
+      console.log('Componente ModificarPerfilPage inicializado');
+    }
+  
+  
+  
+  }
