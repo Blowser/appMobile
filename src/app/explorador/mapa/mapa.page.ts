b13d7a39b467ae4ef2dba239553e72e7d859b83d
Blowser <ig.cortez@duocuc.cl>
2024-11-02 21:10:53 -0300

    Update mapa.page.ts

--- src/app/explorador/mapa/mapa.page.ts
+++ src/app/explorador/mapa/mapa.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
+export class MapaPage {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  navigateTo(page: string) {
+    if (page === 'modificarperfil') {
+      this.router.navigate(['/modificarperfil']);
+    } else if (page === 'inicio') {
+      this.router.navigate(['/home']); // Redirige a la página de inicio
+    }
   }
-
 }
